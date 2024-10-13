766e8733ff3653c2b7e155deef28bc631498c56e
Logan Hobson <Loganhobson31@gmail.com>
2024-10-13 07:01:02 -0500

    Secrets

--- app.py
+++ app.py
@@ -3,32 +3,30 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import pandas as pd
-import json
 
-
-st.title("Wakefield Leaderboard")
-#Initialize Firebase with credentials from Streamlit secrets
-cred = credentials.Certificate(st.secrets["firebase_credentials"])
+cred = credentials.Certificate('WakefieldWeb\.streamlit\secrets.json')  
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://wakefield-scoreboard-default-rtdb.firebaseio.com/'  
+})
 
 def fetch_scores():
-    scores_ref = db.reference('users')  
+    scores_ref = db.reference('users')
     scores = scores_ref.get()
     print(scores)  
     return scores
 
 st.title("High Scores")
-
 scores = fetch_scores()
+
 if scores:
-    
     sorted_scores = sorted(
         [(value['name'], value['score']) for value in scores.values()],
-        key=lambda x: x[1],  
-        reverse=True  
+        key=lambda x: x[1],
+        reverse=True
     )
     score_data = pd.DataFrame(sorted_scores, columns=["Name", "Score"])
 
+    
     st.table(score_data)
-
 else:
     st.write("No scores available.")
